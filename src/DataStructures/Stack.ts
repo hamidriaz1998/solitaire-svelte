9e4fbd29148760b14cbb34622660c5d51035af0f
Hamid <hamidriaz@proton.me>
2024-10-13 21:16:54 +0500

    Add Stack data structure implementation

---
+++ src/DataStructures/Stack.ts
@@ -0,0 +1,27 @@
+export default class Stack<T> {
+    private items: T[];
+    constructor() {
+        this.items = [];
+    }
+    push(item: T) {
+        this.items.push(item);
+    }
+    pop(): T | undefined {
+        return this.items.pop();
+    }
+    peek(): T | undefined {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+    size(): number {
+        return this.items.length;
+    }
+    clear() {
+        this.items = [];
+    }
+    print() {
+        console.log(this.items.toString());
+    }
+}
