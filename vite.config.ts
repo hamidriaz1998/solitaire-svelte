80c29bf97dff1cd184ba342a4f74bb8608369d26
Hamid <hamidriaz@proton.me>
2024-11-09 18:24:00 +0500

    Add workflow to push the build to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 });
